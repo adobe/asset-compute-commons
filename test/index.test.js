3ff683f38802d0951b6c866d1e5adeb065cf22d7
Deirdra Strangio <deirdra.strangio@gmail.com>
2020-05-21 09:59:22 -0600

    Updated eslint and fixed complaints (#9)

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,14 @@
 /*
-Copyright 2020 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
 'use strict';
 
@@ -26,7 +26,7 @@ describe("index.js", function() {
         assert.strictEqual(typeof commons.AssetComputeMetrics, "function");
         Object.keys(AssetComputeErrors).forEach(key => {
             assert.strictEqual(commons[key], AssetComputeErrors[key]);
-        })
+        });
         assert.strictEqual(typeof commons.actionWrapper, "function");
         assert.strictEqual(typeof commons.MetricsTestHelper, "object");
     });
