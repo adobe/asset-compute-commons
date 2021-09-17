9b70a32b76860784abe17c73fe8eec217a58d472
Jamie Delbick <jamie.delbick@gmail.com>
2021-09-17 14:55:37 -0700

    BUGFIX-RELEASE: NUI-1577 extend redacted list to include azure auth object (#62)
    * extend redacted list to include azure auth object
    
    * add another test

--- test/log-utils.test.js
+++ test/log-utils.test.js
@@ -232,6 +232,31 @@ describe("log-utils.js - Credentials redaction", function() {
         assert.equal(redactedObject.testObj.noRedact, "no-redact");
         assert.equal(redactedObject.noRedact, "no-redact-parent");
     });
+    it("redacts rendition-like object", function() {
+        const testObj = {
+            azureAuth: {
+                aadTenantDomain: 'aadTenantDomain',
+                azureClientId: 'azureClientId',
+                azureClientSecret: 'azureClientSecret'
+            },
+            name: 'transcript.vtt',
+            preset: 'video', // or 'audio'
+        };
+
+        const fieldsToRedact = rewiredRedact.__get__("CREDENTIAL_FIELDS_TO_REDACT");
+        const redactField = rewiredRedact.__get__("redactField");
+        const options = [
+            {redactionList: fieldsToRedact, redactionFn: redactField }
+        ];
+
+        const redact = rewiredRedact.__get__("redact");
+
+        const redactedObject = redact(testObj, options, false);
+
+        assert.equal(redactedObject.azureAuth, "[...REDACTED...]");
+        assert.equal(redactedObject.name, "transcript.vtt");
+        assert.equal(redactedObject.preset, "video");
+    });
 
     it("does nothing when not needed", function() {
         const testObj = {};
@@ -703,6 +728,11 @@ describe("log-utils.js - Url redaction", function(){
             "https://adobe80.com:8080/something?query=stuff",
             "https://adobe.80.com:8080/something?query=stuff"
         ];
+        testObj.azureAuth = {
+            aadTenantDomain: 'aadTenantDomain',
+            azureClientId: 'azureClientId',
+            azureClientSecret: 'azureClientSecret'
+        };
 
         AssetComputeLogUtils.log(testObj, "my message");
         AssetComputeLogUtils.log(testObj);
@@ -724,6 +754,7 @@ describe("log-utils.js - Url redaction", function(){
         assert.equal(spyCallArgs.urls[5], "https://adobe1.com:8181");
         assert.equal(spyCallArgs.urls[6], "https://adobe80.com:8080");
         assert.equal(spyCallArgs.urls[7], "https://adobe.80.com:8080");
+        assert.equal(spyCallArgs.azureAuth, "[...REDACTED...]");
 
         console.log.restore();
     });
