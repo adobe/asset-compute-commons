bd874d69d9aebdc96d1286d4de92899a7bb35066
Taylor Callsen <tcallsen@adobe.com>
2022-07-19 10:05:45 -0700

    BUGFIX-RELEASE: ASSETS-13008 Adding activation_start metrics (#76)
    * ASSETS-13008 adding activation_start metrics to be sent in wrap function with main execution
    
    * ASSETS-13008 adjusting promise call for node v10.16
    
    * ASSETS-13008 adjusting activation_start metric sending to fail silently
    
    * ASSETS-13008 reverting changes to activation_start metric error handling - metric failures already handled silently
    
    * ASSETS-13008 adding test to confirm metrics wrapper proceeds if activation_start metric post fails
    
    * ASSETS-13008 commenting and code cleanup per PR feedback

--- test/wrap.test.js
+++ test/wrap.test.js
@@ -35,7 +35,7 @@ describe("wrap", function() {
     });
 
     describe("metrics", function() {
-        it('wraps an action and provides metrics', async function() {
+        it('wraps an action and provides activation and activation_start metrics', async function() {
             const receivedMetrics = MetricsTestHelper.mockNewRelic();
 
             function main(params) {
@@ -66,8 +66,12 @@ describe("wrap", function() {
             assert.equal(result.ok, true);
 
             await MetricsTestHelper.metricsDone();
-            assert.equal(receivedMetrics.length, 1);
+            assert.equal(receivedMetrics.length, 2);
             MetricsTestHelper.assertObjectMatches(receivedMetrics[0], {
+                eventType: "activation_start",
+                timestamp: /\d+/
+            });
+            MetricsTestHelper.assertObjectMatches(receivedMetrics[1], {
                 eventType: "activation",
                 timestamp: /\d+/,
                 duration: /\d+/,
@@ -182,6 +186,55 @@ describe("wrap", function() {
                 duration: /\d+/,
                 my: "metric"
             }]);
+        });
+
+        it('metrics wrapper proceeds gracefully on activation_start metric error', async function() {
+
+            // respond to first NR request with failure
+            nock(MetricsTestHelper.MOCK_BASE_URL)
+                .post(MetricsTestHelper.MOCK_URL_PATH)
+                .reply(500);
+
+            // subsequent NR requests hit regular/successul mock
+            const receivedMetrics = MetricsTestHelper.mockNewRelic();
+
+            function main(params) {
+                assert.equal(typeof params, "object");
+
+                // passed in params
+                assert.equal(params.key, "value");
+
+                // metrics from wrapper
+                assert.equal(typeof params.metrics, "object");
+
+                params.metrics.add({
+                    my: "metric"
+                });
+
+                return { ok: true };
+            }
+
+            const finalMain = actionWrapper(main);
+
+            const params = {
+                newRelicEventsURL: MetricsTestHelper.MOCK_URL,
+                newRelicApiKey: MetricsTestHelper.MOCK_API_KEY,
+                key: "value"
+            };
+
+            const result = await finalMain(params);
+            assert.equal(result.ok, true);
+
+            await MetricsTestHelper.metricsDone();
+
+            // no activation_start metric should be received
+            assert.equal(receivedMetrics.length, 1);
+            MetricsTestHelper.assertObjectMatches(receivedMetrics[0], {
+                eventType: "activation",
+                timestamp: /\d+/,
+                duration: /\d+/,
+                my: "metric"
+            });
         });
     });
 
